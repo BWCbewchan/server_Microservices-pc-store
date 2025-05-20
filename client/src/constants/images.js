4102d635d9a35b4a5484a7337cd7119b1d1f44b2
BWCbewchan <bwc.bewchan@gmail.com>
2025-05-20 17:39:19 +0700

    update git action 7 client(for not find img)

--- client/src/constants/images.js
+++ client/src/constants/images.js
@@ -1,5 +1,5 @@
 import BannerCatalog from "@/assets/images/Banner-Catalog.png";
-import Banner from "@/assets/images/Banner.png";
+import Banner from "@/assets/images/banner.png";
 import CustomeBuilds from "@/assets/images/CustomeBuilds.png";
 import Desktops from "@/assets/images/Desktops.png";
 import GamingMonitors from "@/assets/images/GamingMonitors.png";
